b40777492c37c1ab026833e9e8febe9521a9f325
jhonatan12 <jhoromeropa@gmail.com>
2022-11-07 19:42:26 -0500

    primer avance

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Clients from "./components/clients"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+function App() {
+  
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Clients/>
+    </div>
+  )
+}
+
+export default App
